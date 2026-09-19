bd5096e62190386d6f119be4f3066ffd2de3bcec
LokiLJ <130570447+LokiLJ@users.noreply.github.com>
2026-09-19 01:20:19 -0600

    v3: load custom case study styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 import './v3.css'
+import './special-projects.css'
 
 const redirect = new URLSearchParams(window.location.search).get('redirect')
 if (redirect) {

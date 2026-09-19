96c78ebeae77e8cfc3556b501a39a77e2f1375aa
LokiLJ <130570447+LokiLJ@users.noreply.github.com>
2026-09-18 19:19:52 -0600

    v2: enable browser routing with Pages fallback

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
+const redirect = new URLSearchParams(window.location.search).get('redirect')
+if (redirect) {
+  window.history.replaceState(null, '', redirect)
+}
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )

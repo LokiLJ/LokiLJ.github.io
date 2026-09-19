05910aec1d4768222dff8f2ba35f1cd4e8098821
LokiLJ <130570447+LokiLJ@users.noreply.github.com>
2026-09-19 04:23:16 -0600

    auth: route invite and recovery tokens to setup page

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,11 @@ import './special-projects.css'
 import './special-projects-2.css'
 import './cms.css'
 
+if (window.location.pathname !== '/admin/setup' && /(?:access_token|refresh_token|type=invite|type=recovery)/.test(window.location.hash)) {
+  window.location.replace('/admin/setup' + window.location.hash)
+}
+
+
 const redirect = new URLSearchParams(window.location.search).get('redirect')
 if (redirect) {
   window.history.replaceState(null, '', redirect)

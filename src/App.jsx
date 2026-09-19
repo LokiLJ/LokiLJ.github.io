28d4193f571d7d08871c328f800196ce664aedae
LokiLJ <130570447+LokiLJ@users.noreply.github.com>
2026-09-19 02:25:16 -0600

    cms: add interests and protected admin routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Work from './pages/Work'
 import Project from './pages/Project'
-import Teaching from './pages/Teaching'
 import About from './pages/About'
+import Interests from './pages/Interests'
+import InterestPage from './pages/InterestPage'
+import Admin from './pages/Admin'
 import NotFound from './pages/NotFound'
 
 export default function App() {
@@ -14,8 +16,11 @@ export default function App() {
         <Route index element={<Home />} />
         <Route path="work" element={<Work />} />
         <Route path="work/:projectId" element={<Project />} />
-        <Route path="teaching" element={<Teaching />} />
         <Route path="about" element={<About />} />
+        <Route path="interests" element={<Interests />} />
+        <Route path="interests/:slug" element={<InterestPage />} />
+        <Route path="teaching" element={<Navigate to="/interests/teaching" replace />} />
+        <Route path="admin" element={<Admin />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>

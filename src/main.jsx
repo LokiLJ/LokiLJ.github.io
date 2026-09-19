b20bb0a9dfbbb97f60b69ebdc0aacd2b0c42d69b
LokiLJ <130570447+LokiLJ@users.noreply.github.com>
2026-09-19 00:40:06 -0600

    v3: load production portfolio styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
+import './v3.css'
 
 const redirect = new URLSearchParams(window.location.search).get('redirect')
 if (redirect) {
